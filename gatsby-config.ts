e19bfccebf0becd70c60175320d77ee4233a5ada
pawelklejka <pawel.klejka12653@gmail.com>
2022-09-17 14:25:17 +0200

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,38 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `fountainatlas`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+  // If you use VSCode you can also use the GraphQL plugin
+  // Learn more at: https://gatsby.dev/graphql-typegen
+  graphqlTypegen: true,
+  plugins: [{
+    resolve: 'gatsby-source-contentful',
+    options: {
+      "accessToken": "giU5kFhHQVYRKcbxcYxZXrulLoYNw1A5HobIJC3Qp_U",
+      "spaceId": ""
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-sass", {
+    resolve: 'gatsby-plugin-google-analytics',
+    options: {
+      "trackingId": ""
+    }
+  }, "gatsby-plugin-sitemap", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }, {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};
+
+export default config;
